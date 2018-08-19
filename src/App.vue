408ab4d11cd9b11b752f08fddb28f1e3a759a3e0
Stas <pschnk@gmail.com>
2018-08-18 22:26:36 -0700

    start from lesson1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app-root">
+    <header><h1>{{title}}</h1></header>
+    <div class="puppy-name-grid">
+      <puppy v-for="(puppy_name, index) in names" v-bind:name="puppy_name" v-bind:key="index" v-on:changeURL="updateURL($event)"></puppy>
+    </div>
+    <h2>Hi, I'm {{current_puppy}}</h2>
+    <img v-bind:src="puppy_url" alt="puppy image" />  
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Puppy from './components/Puppy.vue'
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    'puppy': Puppy
+  },
+  data() {
+    return {
+      title: "Hello VUE! Puppy slideshow!",
+      current_puppy: 'Lucy',
+      puppy_url : "http://www.placepuppy.net/1p/760/475",
+      names: ['Lucy', 'Rocky', 'Daisy', 'Buddy', 'Lola', 'Luna', 'Bear', 'Duke', 'Sophie', 'Tucker']
+    }
+  },
+  methods: {
+    updateURL(puppy_name) {
+      this.current_puppy = puppy_name;
+      this.puppy_url = "http://www.placepuppy.net/" + (this.names.indexOf(puppy_name)+1) + "p/760/475";
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Puppy.vue
@@ -0,0 +1,21 @@
+<template>
+  <button v-on:click="changeURL"><h3>{{name}}</h3></button>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+      }
+  },
+  methods: {
+    changeURL(){
+      this.$emit('changeURL', this.name);
+    }
+  }
+}
+</script>
+
+
